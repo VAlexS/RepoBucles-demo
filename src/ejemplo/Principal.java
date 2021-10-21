e1f05a42d2f185595fa0ce614760376490ba1914
VAlexS <vsanzc@alumnos.nebrija.es>
2021-10-21 17:27:52 +0200

    primer commit

---
+++ src/ejemplo/Principal.java
@@ -0,0 +1,11 @@
+package ejemplo;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hola mundo");
+
+        }
+    }
+}
